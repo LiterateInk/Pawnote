45ec44962e2f6d22e306bdaddab12117064e07db
Mikkel RINGAUD <mikkel@milescode.dev>
2023-12-23 19:04:48 +0100

    feat: add authenticate function types

---
+++ src/authenticate/types.ts
@@ -0,0 +1,25 @@
+interface AuthenticateBaseOptions {
+  /**
+   * An unique identifier for the device
+   * that will authenticate to Pronote.
+   */
+  deviceUUID: string
+}
+
+export interface AuthenticatePronoteCredentialsOptions extends AuthenticateBaseOptions {
+  username: string
+  password: string
+}
+
+export interface AuthenticateENTCredentialsOptions extends AuthenticateBaseOptions {
+  entUsername: string
+  entPassword: string
+
+  entURL: string
+  entToken: string
+}
+
+export interface AuthenticateTokenOptions extends AuthenticateBaseOptions {
+  username: string
+  token: string
+}

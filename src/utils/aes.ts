b6a46f7c17731f8d9605eb301b04b35baec1c010
Mikkel RINGAUD <mikkel@milescode.dev>
2024-01-18 10:54:11 +0100

    chore: rename variable names

--- src/utils/aes.ts
+++ src/utils/aes.ts
@@ -1,30 +1,32 @@
 import forge from "node-forge";
 
 class AES {
-  static decrypt(aChaine: string, aCle: forge.util.ByteBuffer, aIv: forge.util.ByteBuffer) {
-    aCle = forge.md.md5.create().update(aCle.bytes()).digest();
-    aIv = aIv.length() ? forge.md.md5.create().update(aIv.bytes()).digest() : forge.util.createBuffer().fillWithByte(0, 16);
-    const aChaineBuffer = forge.util.createBuffer(forge.util.hexToBytes(aChaine));
-    const lChiffreur = forge.cipher.createDecipher("AES-CBC", aCle);
-    lChiffreur.start({
-      iv: aIv
-    });
-    lChiffreur.update(aChaineBuffer);
-    lChiffreur.finish();
-    return lChiffreur.output.bytes();
+  static decrypt(data: string, key: forge.util.ByteBuffer, iv: forge.util.ByteBuffer) {
+    key = forge.md.md5.create().update(key.bytes()).digest();
+    iv = iv.length() ? forge.md.md5.create().update(iv.bytes()).digest() : forge.util.createBuffer().fillWithByte(0, 16);
+    
+    const buffer = forge.util.createBuffer(forge.util.hexToBytes(data));
+    const decipher = forge.cipher.createDecipher("AES-CBC", key);
+    
+    decipher.start({ iv });
+    decipher.update(buffer);
+    decipher.finish();
+    
+    return decipher.output.bytes();
   }
 
-  static encrypt(aChaine: string | ArrayBuffer | forge.util.ArrayBufferView | forge.util.ByteStringBuffer, aCle: forge.util.ByteBuffer, aIv: forge.util.ByteBuffer) {
-    aCle = forge.md.md5.create().update(aCle.bytes()).digest();
-    aIv = aIv.length() ? forge.md.md5.create().update(aIv.bytes()).digest() : forge.util.createBuffer().fillWithByte(0, 16);
-    aChaine = forge.util.createBuffer(aChaine);
-    const lChiffreur = forge.cipher.createCipher("AES-CBC", aCle);
-    lChiffreur.start({
-      iv: aIv
-    });
-    lChiffreur.update(aChaine);
-    lChiffreur.finish();
-    return lChiffreur.output.toHex();
+  static encrypt(data: string | ArrayBuffer | forge.util.ArrayBufferView | forge.util.ByteStringBuffer, key: forge.util.ByteBuffer, iv: forge.util.ByteBuffer) {
+    key = forge.md.md5.create().update(key.bytes()).digest();
+    iv = iv.length() ? forge.md.md5.create().update(iv.bytes()).digest() : forge.util.createBuffer().fillWithByte(0, 16);
+    
+    data = forge.util.createBuffer(data);
+    const cipher = forge.cipher.createCipher("AES-CBC", key);
+    
+    cipher.start({ iv });
+    cipher.update(data);
+    cipher.finish();
+    
+    return cipher.output.toHex();
   }
 }
 

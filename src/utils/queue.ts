b15d5d9e10b610fb2d8720d6efcf527ca3f76e1c
Mikkel RINGAUD <mikkel@milescode.dev>
2024-01-21 22:05:14 +0100

    fix(queue): use proper variables and add credits

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -4,10 +4,12 @@ interface QueueItem {
   reject: (err?: any) => void
 }
 
+/// Stolen from <https://medium.com/@karenmarkosyan/how-to-manage-promises-into-dynamic-queue-with-vanilla-javascript-9d0d1f8d4df5>
+/// Available at this gist : <https://gist.github.com/ogostos/a04869d68313ee51f496b2996026d8ed#file-five-js>
+/// Thanks you, [@ogostos](github.com/ogostos)
 export default class Queue {
-  queue: QueueItem[] = [];
-  pendingPromise = false;
-  workingOnPromise = false;
+  private queue: QueueItem[] = [];
+  private pendingPromise = false;
 
   public push<T extends unknown>(promise: () => Promise<T>) {
     return new Promise<T>((resolve, reject) => {
@@ -22,28 +24,28 @@ export default class Queue {
   }
 
   private dequeue() {
-    if (this.workingOnPromise) return false;
+    if (this.pendingPromise) return false;
 
     const item = this.queue.shift();
     if (!item) return false;
 
     try {
-      this.workingOnPromise = true;
+      this.pendingPromise = true;
 
       item.promise()
         .then((value) => {
-          this.workingOnPromise = false;
+          this.pendingPromise = false;
           item.resolve(value);
           this.dequeue();
         })
         .catch((err) => {
-          this.workingOnPromise = false;
+          this.pendingPromise = false;
           item.reject(err);
           this.dequeue();
         });
     }
     catch (err) {
-      this.workingOnPromise = false;
+      this.pendingPromise = false;
       item.reject(err);
       this.dequeue();
     }

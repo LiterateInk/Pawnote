1d5e900e6c03c2cbd8f56ee5150fd52292991bac
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-22 07:09:21 +0100

    feat: add ApiUserMessageRecipients

---
+++ src/api/user/messageRecipients/types.ts
@@ -0,0 +1,49 @@
+import type { PronoteValue } from "~/api/type";
+import type { PronoteApiFunctions } from "~/constants/functions";
+import type { PronoteApiOnglets } from "~/constants/onglets";
+import type { PronoteApiUserMessageRecipientType } from "~/constants/recipients";
+import type { Session } from "~/session";
+
+export interface PronoteApiUserMessageRecipients {
+  request: {
+    _Signature_: {
+      onglet: PronoteApiOnglets.Discussions
+    }
+    donnees: {
+      message: {
+        N: string
+      }
+
+      estPublicParticipant: boolean
+      estDestinatairesReponse: boolean
+    }
+  }
+
+  response: {
+    nom: PronoteApiFunctions.DiscussionsRecipients
+    donnees: {
+      listeDest: PronoteValue<24, Array<{
+        L: string
+        N: string
+        /**
+         * Type of the recipient.
+         * Tells whether the recipient is a teacher, student, ...
+         */
+        G: PronoteApiUserMessageRecipientType
+        P: number
+        refusMess?: boolean
+      }>>
+    }
+  }
+}
+
+export interface ApiUserMessageRecipients {
+  input: {
+    session: Session
+    messageID: string
+  }
+
+  output: {
+    data: PronoteApiUserMessageRecipients["response"]
+  }
+}

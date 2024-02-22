1bcfcb2250ea58f35740855b0d4b57d2d5f271a5
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-22 07:10:15 +0100

    feat(api/messages): add types for `listeDocumentsJoints`
    also refactored most of `{ _T, V }` objects to `PronoteValue<>` helper type

--- src/api/user/messages/types.ts
+++ src/api/user/messages/types.ts
@@ -3,6 +3,7 @@ import type { PronoteApiFunctions } from "~/constants/functions";
 import type { PronoteApiOnglets } from "~/constants/onglets";
 import type { Session } from "~/session";
 import type { PronoteApiMessagesPossessionsList } from "~/constants/messages";
+import type { PronoteApiAttachment } from "~/constants/attachments";
 
 export interface PronoteApiUserMessages {
   request: {
@@ -17,86 +18,84 @@ export interface PronoteApiUserMessages {
 
   response: {
     donnees: {
-      listeMessages: {
-        _T: 24
-        V: Array<{
+      listeMessages: PronoteValue<24, Array<{
+        N: string
+
+        messageSource: PronoteValue<24, {
           N: string
-
-          messageSource: {
-            _T: 24
-            V: {
-              N: string
-            }
-          }
-
-          possessionMessage: {
-            _T: 24
-            V: {
-              N: string
-            }
-          }
-
-          /** NOTE: No idea what this is. */
-          estNonPossede: boolean
-
-          /**
-           * Another format of date, why ?
-           * @example "ven. 16/02/24 13h35"
-           */
-          libelleDate: string
-
-          /** @example "16/02/2024 13:35:11" */
-          date: PronoteValue<7, string>
-
-          estUnAparte: boolean
-
-          /**
-           * You're the one who sent the message.
-           */
-          emetteur: boolean
-
-          /**
-           * The one sending the message.
-           * @example "Moi" // in this case, `message.emetteur === true`
-           */
-          public_gauche: string
-          /**
-           * The one receiving the message.
-           * @example "John D."
-           */
-          public_droite: string
-          /**
-           * @example "Professeur"
-           */
-          hint_droite: string
-        } & (
-          | {
-            contenu: string
-            estHTML: false
-          }
-          | {
-            contenu: PronoteValue<number, string>;
-            estHTML: true
-          }
-        )>
-      }
-
-      messagePourReponse: {
-        _T: number
-        V: {
-          N: string
-          G: number
-          nbDestinataires: number
-        }
-      }
-
-      listeBoutons: {
-        _T: number
-        V: Array<{
-          G: number
-          L: string
         }>
-      }
+
+        possessionMessage: PronoteValue<24, {
+          N: string
+        }>
+
+        /** NOTE: No idea what this is. */
+        estNonPossede: boolean
+
+        /**
+         * Another format of date, why ?
+         * @example "ven. 16/02/24 13h35"
+         */
+        libelleDate: string
+
+        /** @example "16/02/2024 13:35:11" */
+        date: PronoteValue<7, string>
+
+        listeDocumentsJoints?: PronoteValue<24, Array<PronoteApiAttachment>>
+
+        /**
+         * You're the one who sent the message.
+         */
+        emetteur: boolean
+
+        /**
+         * The one sending the message.
+         * @example "Moi" // in this case, `message.emetteur === true`
+         */
+        public_gauche: string
+
+        /**
+         * The one receiving the message.
+         * Can be undefined when sending to a lot of people at the same time.
+         * When this happens, you can see how much people received the message in `nbPublic`.
+         * @example "John D."
+         */
+        public_droite?: string
+
+        /**
+         * @example "Professeur"
+         */
+        hint_droite?: string
+
+        /**
+         * @example "Professeur"
+         */
+        hint_gauche: string
+
+        estUnAparte: boolean
+        /** Shows all the recipients the message was sent to. */
+        nbPublic?: number
+      } & (
+        | {
+          contenu: string
+          estHTML: false
+        }
+        | {
+          contenu: PronoteValue<number, string>;
+          estHTML: true
+        }
+      )>>
+
+      messagePourReponse: PronoteValue<24, {
+        N: string
+        G: number
+        nbDestinataires: number
+      }>
+
+      listeBoutons: PronoteValue<24, Array<{
+        G: number
+        L: string
+      }>>
 
       nbPossessionsMessage: number
     }

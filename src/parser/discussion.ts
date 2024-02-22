640e6e525e806d603542e8ea22b2fbaae563ff1a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-22 07:08:49 +0100

    feat(StudentDiscussion): add folders
    - also includes a rename for hourString because the naming was kinda false, so now it's dateAsFrenchText and added a documentation for it

--- src/parser/discussion.ts
+++ src/parser/discussion.ts
@@ -11,14 +11,14 @@ export class StudentDiscussionsOverview {
     this.folders = data.listeEtiquettes.V.map((folder) => new StudentDiscussionFolder(folder));
     this.discussions = data.listeMessagerie.V
       .filter((discussion) => discussion.estUneDiscussion && (discussion.profondeur || 0) === 0)
-      .map((discussion) => new StudentDiscussion(client, discussion));
+      .map((discussion) => new StudentDiscussion(client, discussion, this.folders));
   }
 }
 
 export class StudentDiscussionFolder {
-  public id: string;
-  public name: string;
-  public genre: number;
+  public readonly id: string;
+  public readonly name: string;
+  public readonly genre: number;
 
   constructor (data: PronoteApiUserDiscussions["response"]["donnees"]["listeEtiquettes"]["V"][number]) {
     this.id = data.N;
@@ -35,25 +35,38 @@ export class StudentDiscussion {
   public possessions: PronoteApiMessagesPossessionsList;
 
   public read: boolean;
-  public hourString?: string;
+
+  /**
+   * Output is very variable, see example below.
+   * Because of this behavior, we can't transform this into a date.
+   * Maybe, we could parse this manually, but it's not a priority.
+   * TODO: Parse this manually.
+   *
+   * @example
+   * "lundi 08h53"
+   * // or can just be the hour
+   * "07h26"
+   */
+  public dateAsFrenchText: string;
   public subject: string;
   public recipientName?: string;
   public numberOfMessages: number;
   public numberOfMessagesUnread: number;
   public creator?: string;
   public closed: boolean;
+  public folders: StudentDiscussionFolder[];
 
-  constructor (private client: Pronote, data: PronoteApiUserDiscussions["response"]["donnees"]["listeMessagerie"]["V"][number]) {
+  constructor (private client: Pronote, data: PronoteApiUserDiscussions["response"]["donnees"]["listeMessagerie"]["V"][number], folders: StudentDiscussionFolder[]) {
     this.possessions = data.listePossessionsMessages.V;
     this.read = data.lu ?? false;
-    this.hourString = data.libelleDate;
+    this.dateAsFrenchText = data.libelleDate!;
     this.subject = data.objet!; // since filtered, object is always here.
     this.recipientName = data.public;
     this.numberOfMessages = data.nombreMessages ?? 0;
     this.numberOfMessagesUnread = data.nbNonLus ?? 0;
     this.creator = data.initiateur;
     this.closed = data.ferme ?? false;
-    // TODO: ListeEtiquettes
+    this.folders = data.listeEtiquettes?.V.map((folder) => folders.find((f) => f.id === folder.N)!) ?? [];
   }
 
   public fetchMessages (): Promise<StudentMessage[]> {

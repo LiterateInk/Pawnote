ebf0db78fdbf547f31f78ec389303cf9e5b81d9c
Mikkel RINGAUD <mikkel@milescode.dev>
2024-03-11 09:24:51 +0100

    docs: fix directory structure

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -29,6 +29,12 @@ export default defineConfig({
           autogenerate: {
             directory: "contributing"
           }
+        },
+        {
+          label: "Pronote Internals",
+          autogenerate: {
+            directory: "pronote-internals"
+          }
         }
       ],
 

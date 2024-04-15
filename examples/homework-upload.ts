55bf54e423c8dc30f1e9fcb34ac3d4aea49189f6
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-15 23:43:07 +0200

    docs(homework): add file upload example

---
+++ examples/homework-upload.ts
@@ -0,0 +1,40 @@
+import { authenticatePronoteCredentials, PronoteApiAccountId, PronoteApiHomeworkReturnType } from "../src";
+
+(async () => {
+  const pronote = await authenticatePronoteCredentials("https://pronote-vm.dev/pronote", {
+    accountTypeID: PronoteApiAccountId.Student,
+    username: "lisa.boulanger",
+    password: "12345678",
+
+    // Because this is just an example, don't forget to change this.
+    deviceUUID: "my-device-uuid"
+  });
+
+  const homework = await pronote.getHomeworkForInterval(new Date());
+
+  console.log("---");
+
+  for (const item of homework) {
+    // Output something...
+    console.log(item.subject.name, "to finish before the", item.deadline.toLocaleString());
+    console.log("(description) =>", item.description);
+
+    if (item.return) {
+      if (item.return.type === PronoteApiHomeworkReturnType.PAPER) {
+        console.log("(return) => on paper ; should be returned to teacher manually");
+      }
+      else if (item.return.type === PronoteApiHomeworkReturnType.FILE_UPLOAD) {
+        console.log("(return) => file upload", item.return.uploaded ? "(uploaded)" : "(not uploaded)");
+
+        if (item.return.canUpload) {
+          const fileName = "hello-world.txt";
+          const fileContent = Buffer.from(`Hello, world! This file was sent by Pawnote, the ${Date.now()} !`, "utf8");
+          await item.uploadFile(fileContent, fileName);
+          console.info("(upload) => successfully uploaded", fileName);
+        }
+      }
+    }
+
+    console.log("---");
+  }
+})();

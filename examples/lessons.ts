ec9e01aae35493f38eb6ace8083121be84564360
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-18 00:31:50 +0200

    docs(examples):: implement new timetable behavior with activity and lesson separation

--- examples/lessons.ts
+++ examples/lessons.ts
@@ -1,4 +1,4 @@
-import { authenticatePronoteCredentials, PronoteApiAccountId } from "../src";
+import { authenticatePronoteCredentials, PronoteApiAccountId, TimetableActivity, TimetableLesson } from "../src";
 
 (async () => {
   const pronote = await authenticatePronoteCredentials("https://pronote-vm.dev/pronote", {
@@ -10,24 +10,42 @@ import { authenticatePronoteCredentials, PronoteApiAccountId } from "../src";
     deviceUUID: "my-device-uuid"
   });
 
-  const from = new Date("2024-04-17");
-  // const to = new Date("2023-09-20");
+  const overview = await pronote.getTimetableOverview(new Date("2024-04-15"));
+  const lessons = overview.parseLessons({
+    withSuperposedCanceledLessons: false,
+    withCanceledLessons: true,
+    withPlannedLessons: true
+  });
 
-  const lessons = await pronote.getLessonsForInterval(from);
-  lessons.forEach((lesson) => {
-    // Let's use "???" as a placeholder when there's no subject on a lesson.
-    const subjectName = lesson.subject?.name ?? "???";
-
-
-    console.log(subjectName, "starts the", lesson.start.toLocaleString(), "and ends the", lesson.end.toLocaleString());
-    console.info("(teachers) =>", lesson.teacherNames.join(", ") || "none");
-    console.info("(classrooms) =>", lesson.classrooms.join(", ") || "none");
-    console.info("(groups) =>", lesson.groupNames.join(", ") || "none");
-    console.info("(personal) =>", lesson.personalNames.join(", ") || "none");
-
-    if (lesson.haveLessonResource) {
-      console.log("(info) => has lesson resource/content");
+  lessons.forEach((item) => {
+    if (item instanceof TimetableActivity) {
+      console.log(item.title, "starts the", item.startDate.toLocaleString(), "ends the", item.endDate.toLocaleString());
+      console.log("(attendants) =>", item.attendants.join(", ") || "none");
+      console.log(`(resourceType->${item.resourceTypeName}) => ${item.resourceValue}`);
     }
+    else if (item instanceof TimetableLesson) {
+      // Let's use "???" as a placeholder when there's no subject on a lesson.
+      const subjectName = item.subject?.name ?? "???";
+      console.log(subjectName, "starts the", item.startDate.toLocaleString(), "ends the", item.endDate.toLocaleString());
+
+      console.log("(teachers) =>", item.teacherNames.join(", ") || "none");
+      console.log("(classrooms) =>", item.classrooms.join(", ") || "none");
+      console.log("(groups) =>", item.groupNames.join(", ") || "none");
+      console.log("(personal) =>", item.personalNames.join(", ") || "none");
+
+      if (item.lessonResourceID) {
+        console.log("(info) => has lesson resource/content");
+      }
+
+      if (item.status) {
+        console.log("(status) =>", item.status);
+      }
+
+      console.log("(detention) =>", item.detention);
+      console.log("(exempted) =>", item.exempted);
+    }
+
+    console.info("(notes) =>", item.notes || "none");
 
     // Break line.
     console.log();

62fd657036191e3fee1015d4b108d119c566cb73
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-20 19:02:19 +0200

    fix(pronote): do not follow redirects when downloading portal page

--- lib/src/pronote/login_portal_dowloader.dart
+++ lib/src/pronote/login_portal_dowloader.dart
@@ -1,14 +1,21 @@
 import 'package:http/http.dart' as http;
 import '../constants/user_agent.dart';
 
-void loginPortalDownloader (Uri uri, Set<String> cookies) async {
-  final loginPortalHtml = await http.read(uri,
-    headers: {
+Future<String> loginPortalDownloader (Uri uri, Set<String> cookies) async {
+  final client = http.Client();
+
+  final request = http.Request("GET", uri)
+    ..followRedirects = false
+    ..headers.addAll({
       'Cookie': cookies.join('; '),
       'User-Agent': internalUserAgent
-    }
-  );
+    });
 
-  // TODO: Manual redirect ???
-  print(loginPortalHtml);
+  try {
+    final loginPortalResponse = await client.send(request);
+    return loginPortalResponse.stream.bytesToString();
+  }
+  finally {
+    client.close();
+  }
 }

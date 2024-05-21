41ca76607150bf60535dce82dcfdc82835ef46f2
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-21 08:16:21 +0200

    feat(session): add payload writer/reader

--- lib/src/models/session.dart
+++ lib/src/models/session.dart
@@ -1,10 +1,8 @@
-import 'package:encrypt/encrypt.dart';
-
 import '../constants/accounts.dart';
-import '../constants/rsa.dart';
-
-import 'session_metadata.dart';
+import 'function_payload.dart';
 import 'session_encryption.dart';
+import 'session_encryption_aes.dart';
+import 'session_metadata.dart';
 
 class Session {
   SessionMetadata metadata;
@@ -16,7 +14,7 @@ class Session {
     required this.encryption,
   });
 
-  factory Session.fromJson(Map<String, dynamic> json) {
+  factory Session.fromJson(Map<String, dynamic> json, Uri instanceRootUri) {
     // Make sure the JSON is valid with the minimal required keys.
     if (json case {
       'h': String sessionID,
@@ -25,8 +23,8 @@ class Session {
       final accountType = ApiAccountType.values.firstWhere((element) => element.id == accountTypeId);
       
       // Fallback to latest version defaults.
-      String rsaExponent = defaultRsaModulo1024;
-      String rsaModulus = defaultRsaExponent1024;
+      String rsaModulus = defaultRsaModulus1024;
+      String rsaExponent = defaultRsaExponent1024;
       PronoteVersion version = PronoteVersion.v2023;
 
       // Before the v2023 update, we would have those two parameters for RSA.
@@ -37,14 +35,8 @@ class Session {
         version = PronoteVersion.vBefore2023;
       }
 
-      IV? aesIv;
-
-      // We have to setup IV for our session when we're not in `common`.
-      if (accountType != ApiAccountType.common) {
-        aesIv = IV.fromSecureRandom(16);
-      }
-
       final metadata = SessionMetadata(
+        instanceRootUri: instanceRootUri,
         sessionID: sessionID,
         accountType: accountType,
         skipCompression: json["sCoA"] ?? false,
@@ -55,9 +47,9 @@ class Session {
       );
 
       final encryption = SessionEncryption(
-        rsaExponent: rsaExponent,
+        aes: SessionEncryptionAes(),
         rsaModulus: rsaModulus,
-        aesIv: aesIv,
+        rsaExponent: rsaExponent,
       );
 
       return Session(
@@ -68,5 +60,33 @@ class Session {
     else {
       throw FormatException('Invalid JSON: $json');
     }
+  }
+
+  FunctionPayload writeFunctionPayload (Map<String, dynamic> payload) {
+    order++;
+
+    String encryptedOrder = encryption.aes.encrypt(order.toString());
+    // TODO: Compression and encryption.
+    
+    return FunctionPayload(encryptedOrder: encryptedOrder, data: payload);
+  }
+
+  Map<String, dynamic> readFunctionPayload (FunctionPayload payload) {
+    order++;
+    final decryptedOrder = encryption.aes.decrypt(payload.encryptedOrder);
+
+    if (decryptedOrder != order.toString()) {
+      throw Exception('Invalid order: $decryptedOrder');
+    }
+
+    if (payload.data is String) {
+      // TODO: Decompression and decryption.
+    }
+
+    return payload.data;
+  }
+
+  void writeFileUploadPayload () {
+    // TODO
   }
 }

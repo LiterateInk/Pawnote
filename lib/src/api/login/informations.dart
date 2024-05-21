391868ab37fc35b298f4061954cf52ab25fc9399
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-21 08:17:18 +0200

    feat(api): add login.informations

--- lib/src/api/login/informations.dart
+++ lib/src/api/login/informations.dart
@@ -1,4 +1,22 @@
-import '../../constants/accounts.dart';
+import 'package:encrypt/encrypt.dart';
 
-void callApiLoginInformations (Uri pronoteUri, ApiAccountType accountType, Set<String> pronoteCookies) async {
+import '../../constants/api_functions_name.dart';
+import '../../models/session.dart';
+import '../../pronote/request_function_api.dart';
+
+Future<Map<String, dynamic>> callApiLoginInformations (Session session, {
+  required String uuid,
+  required IV ivAfterInitialization
+}) async {
+  final requestPayload = session.writeFunctionPayload({
+    "donnees": {
+      "identifiantNav": null,
+      "Uuid": uuid
+    }
+  });
+
+  final responsePayload = await requestFunctionApi(session, requestPayload, ApiFunctionsName.informations);
+  session.encryption.aes.iv = ivAfterInitialization;
+  
+  return session.readFunctionPayload(responsePayload);
 }

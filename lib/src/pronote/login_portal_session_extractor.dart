b815e0b9740d2f21acba17627feda5e4649429aa
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-21 08:12:45 +0200

    fix: add instance root uri to session extractor
    for later usage

--- lib/src/pronote/login_portal_session_extractor.dart
+++ lib/src/pronote/login_portal_session_extractor.dart
@@ -1,23 +1,23 @@
 import '../models/session.dart';
 import '../utils/relaxed_json_to_json.dart';
 
-Session loginPortalSessionExtractor (String body) {
+Session loginPortalSessionExtractor (String loginPortalHtml, Uri instanceRootUri) {
   // 1. Remove all spaces and line breaks.
-  body = body.replaceAll(RegExp(r'\s'), '');
+  loginPortalHtml = loginPortalHtml.replaceAll(RegExp(r'\s'), '');
 
   // Define the start and end of the relaxed JSON position.
   const from = "Start(";
   const to = ")}catch";
 
   // 2. Extract the relaxed JSON of the session.
-  final relaxed = body.substring(
-    body.indexOf(from) + from.length,
-    body.indexOf(to)
+  final relaxed = loginPortalHtml.substring(
+    loginPortalHtml.indexOf(from) + from.length,
+    loginPortalHtml.indexOf(to)
   );
 
   // 3. Convert the relaxed JSON to a valid JSON.
   final json = relaxedJsonToJson(relaxed);
 
   // 4. Give the extracted session.
-  return Session.fromJson(json);
+  return Session.fromJson(json, instanceRootUri);
 }

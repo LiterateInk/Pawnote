f0ddf01bff3b748709d2c5bfefdce2b22c0eb466
Mikkel ALMONTE--RINGAUD <mikkel@milescode.dev>
2024-07-16 01:49:57 +0200

    feat: implement `retrieve_pronote_root_url` method

--- pawnote/src/lib.rs
+++ pawnote/src/lib.rs
@@ -1,3 +1,30 @@
-pub fn hello () -> String {
-  String::from("Hello, world!")
+use url::Url;
+
+pub fn retrieve_pronote_root_url (pronote_url: String) -> String {
+  let url = Url::parse(&pronote_url).unwrap();
+  let origin = url.origin().ascii_serialization();
+  let mut paths: Vec<&str> = url.path().split('/').collect();
+  
+  if let Some(last_path) = paths.last() {
+    // Remove the last part of the path if ending with a ".json" or ".html".
+    if last_path.ends_with(".json") || last_path.ends_with(".html") {
+      let _ = paths.pop();
+    }
+  }
+
+  // Remove empty paths (will also remove the first '/')
+  paths.retain(|&x| !x.is_empty());
+
+  // Join the paths back together ("pronote/eleve.html")
+  let path_name = paths.join("/");
+
+  if !path_name.is_empty() {
+    // Put everything back, just add the '/' we removed
+    // earlier before the actual path.
+    origin + "/" + &path_name
+  }
+  else {
+    // Path is the root, so no need to append it.
+    origin
+  }
 }

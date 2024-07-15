bb8a7f4076c8cc8966403c1257ab861ca8f8e7ca
Mikkel ALMONTE--RINGAUD <mikkel@milescode.dev>
2024-07-16 01:50:40 +0200

    chore: add example

--- pawnote/examples/hello.rs
+++ pawnote/examples/hello.rs
@@ -1,5 +1,7 @@
 extern crate pawnote;
 
 fn main() {
-  println!("{}", pawnote::hello());
+  println!("{}", pawnote::retrieve_pronote_root_url(String::from("https://demo.index-education.net/pronote////eleve.html?fd=1")));
+  println!("{}", pawnote::retrieve_pronote_root_url(String::from("https://demo.index-education.net////eleve.html?fd=1")));
+  println!("{}", pawnote::retrieve_pronote_root_url(String::from("https://demo.index-education.net////eleve/hehe/l.html?fd=1")));
 }

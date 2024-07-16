95ca2350ce0dd660eee7cb56ca7893838e52c696
Mikkel RINGAUD <mikkel.almonteringaud@legrand.com>
2024-07-16 09:42:48 +0200

    feat: add web spaces according to PRONOTE constants

--- pawnote/examples/hello.rs
+++ pawnote/examples/hello.rs
@@ -2,6 +2,7 @@ extern crate pawnote;
 
 fn main() {
   println!("{}", pawnote::retrieve_pronote_root_url(String::from("https://demo.index-education.net/pronote////eleve.html?fd=1")));
-  println!("{}", pawnote::retrieve_pronote_root_url(String::from("https://demo.index-education.net////eleve.html?fd=1")));
-  println!("{}", pawnote::retrieve_pronote_root_url(String::from("https://demo.index-education.net////eleve/hehe/l.html?fd=1")));
+  println!("{}", pawnote::retrieve_pronote_root_url(String::from("https://demo.index-education.net/eleve.html?fd=1")));
+
+  println!("{}", pawnote::WebSpace::Students.to_path());
 }
--- pawnote/src/lib.rs
+++ pawnote/src/lib.rs
@@ -28,3 +28,47 @@ pub fn retrieve_pronote_root_url (pronote_url: String) -> String {
     origin
   }
 }
+
+pub enum WebSpace {
+  Students = 6, // = Élèves
+  
+  /// This web space is currently not supported in Pawnote.
+  Parents = 7,
+  
+  /// This web space is currently not supported in Pawnote.
+  Teachers = 8, // = Professeurs
+
+  /// This web space is currently not supported in Pawnote.
+  Administration = 14, // = Vie Scolaire
+  
+  /// This web space is currently not supported in Pawnote.
+  Management = 17, // = Direction
+  
+  /// This web space is currently not supported in Pawnote.
+  Assistants = 26 // = Accompagnants
+}
+
+impl WebSpace {
+  pub fn from_id (id: u8) -> Option<WebSpace> {
+    match id {
+      6 => Some(WebSpace::Students),
+      7 => Some(WebSpace::Parents),
+      8 => Some(WebSpace::Teachers),
+      26 => Some(WebSpace::Assistants),
+      14 => Some(WebSpace::Administration),
+      17 => Some(WebSpace::Management),
+      _ => None,
+    }
+  }
+
+  pub fn to_path (&self) -> String {
+    match self {
+      WebSpace::Students => "mobile.eleve.html".into(),
+      WebSpace::Parents => "mobile.parent.html".into(),
+      WebSpace::Teachers => "mobile.professeur.html".into(),
+      WebSpace::Assistants => "mobile.accompagnant.html".into(),
+      WebSpace::Administration => "mobile.viescolaire.html".into(),
+      WebSpace::Management => "mobile.direction.html".into(),
+    }
+  }
+}

699eab4642d61e87b6bb2f2ed6f36edc2aeefc6f
Mikkel RINGAUD <mikkel.almonteringaud@legrand.com>
2024-07-16 15:56:51 +0200

    docs: move examples to sub-crates

---
+++ examples/authenticate/src/main.rs
@@ -0,0 +1,23 @@
+#[tokio::main]
+async fn main() {
+  println!("{}", pawnote::WebSpace::Students.to_path());
+
+  let response = pawnote::authenticate_with_credentials(
+    "https://google.com".to_string(),
+    pawnote::WebSpace::Students,
+    "username".to_string(),
+    "password".to_string(),
+    "device_uuid".to_string(),
+    utilities::reqwest_fetcher
+  ).await;
+
+  match response {
+    Ok(result) => {
+      let headers_string = result.headers.iter().map(|(k, v)| format!("[ \"{}\", \"{}\" ]", k, v)).collect::<Vec<String>>().join(", ");
+      println!("Async operation successful: {}", headers_string);
+    },
+    Err(err) => {
+        eprintln!("Async operation failed: {}", err);
+    }
+  }
+}

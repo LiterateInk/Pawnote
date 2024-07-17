4d40d1852b5ef927bb4c100ca37a88a45016a5a4
Mikkel ALMONTE--RINGAUD <mikkel@milescode.dev>
2024-07-18 01:55:57 +0200

    chore: update examples with new output

--- examples/javascript/authenticate.ts
+++ examples/javascript/authenticate.ts
@@ -1,6 +1,6 @@
 import { WebSpace, authenticate_with_credentials } from "pawnote";
 
-const response = await authenticate_with_credentials(
+const session = await authenticate_with_credentials(
   "https://demo.index-education.net/pronote",
   WebSpace.Students,
   "demonstration",
@@ -8,4 +8,17 @@ const response = await authenticate_with_credentials(
   "device-uuid",
 );
 
-console.log(response);
+console.log({
+  id: session.id,
+  web_space: session.web_space,
+  demo: session.demo,
+  ent_username: session.ent_username,
+  ent_password: session.ent_password,
+  access_type: session.access_type,
+  modulus_rsa: session.modulus_rsa,
+  exponent_rsa: session.exponent_rsa,
+  skip_encryption: session.skip_encryption,
+  skip_compression: session.skip_compression,
+  http: session.http,
+  poll: session.poll,
+});
--- examples/rust/authenticate/src/main.rs
+++ examples/rust/authenticate/src/main.rs
@@ -1,18 +1,16 @@
 #[tokio::main]
 async fn main() {
-  println!("{}", pawnote::WebSpace::Students.to_path());
-
   let response = pawnote::authenticate_with_credentials(
-    "https://demo.index-education.net/pronote".to_string(),
+    String::from("https://demo.index-education.net/pronote"),
     pawnote::WebSpace::Students,
-    "username".to_string(),
-    "password".to_string(),
-    "device_uuid".to_string()
+    String::from("demonstration"),
+    String::from("pronotevs"),
+    String::from("device-uuid")
   ).await;
 
   match response {
     Ok(result) => {
-      println!("Async operation successful: {}", result.content);
+      println!("{}", serde_json::to_string_pretty(&result).unwrap());
     },
     Err(err) => {
         eprintln!("Async operation failed: {}", err);

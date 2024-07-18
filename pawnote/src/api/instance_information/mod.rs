4df9973bc5755a3e22cfa7d5be02cbbec66f8b55
Mikkel RINGAUD <mikkel.almonteringaud@legrand.com>
2024-07-18 15:56:05 +0200

    docs(api): add comments for TODO

---
+++ pawnote/src/api/instance_information/mod.rs
@@ -0,0 +1,14 @@
+//! Makes a request to the infoMobileApp.json endpoint in order to get
+//! some information about an instance without having to log in.
+//! 
+//! For example, we can use it to know if the instance has a "CAS"
+//! set up or not. We can also grab the currently available webspaces
+//! and the instance PRONOTE version.
+
+// TODO
+// mod request;
+// TODO
+// mod response;
+
+// pub use request::build_request;
+// pub use response::parse_response;
--- pawnote/src/api/instance_parameters/mod.rs
+++ pawnote/src/api/instance_parameters/mod.rs
@@ -1,5 +1,13 @@
-mod request;
-mod response;
+//! First request to make to the PRONOTE API
+//! after creating a PRONOTE session.
+//! 
+//! It sets up the AES encryption key for the authentication
+//! requests that will follow.
 
-pub use request::build_request;
-pub use response::parse_response;
+// TODO
+// mod request;
+// TODO
+// mod response;
+
+// pub use request::build_request;
+// pub use response::parse_response;

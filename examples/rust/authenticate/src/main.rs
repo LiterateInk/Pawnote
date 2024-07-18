b6edba34bfe59cf241cc8d2af05a2a8ec259e22a
Mikkel RINGAUD <mikkel.almonteringaud@legrand.com>
2024-07-18 12:55:24 +0200

    docs(examples/rust): add mem usage, uuid and unwrap

--- examples/rust/authenticate/src/main.rs
+++ examples/rust/authenticate/src/main.rs
@@ -1,19 +1,33 @@
+use peak_alloc::PeakAlloc;
+use uuid::Uuid;
+
+use pawnote::{
+  WebSpace,
+  authenticate_with_credentials
+};
+
+#[global_allocator]
+static PEAK_ALLOC: PeakAlloc = PeakAlloc;
+
 #[tokio::main]
 async fn main() {
-  let response = pawnote::authenticate_with_credentials(
-    String::from("https://demo.index-education.net/pronote"),
-    pawnote::WebSpace::Students,
-    String::from("demonstration"),
-    String::from("pronotevs"),
-    String::from("device-uuid")
-  ).await;
+  let pronote_url = String::from("https://demo.index-education.net/pronote");
+  let username = String::from("demonstration");
+  let password = String::from("pronotevs");
+  let device_uuid = Uuid::new_v4();
 
-  match response {
-    Ok(result) => {
-      println!("{}", serde_json::to_string_pretty(&result).unwrap());
-    },
-    Err(err) => {
-        eprintln!("Async operation failed: {}", err);
-    }
-  }
+  let pronote_session = authenticate_with_credentials(
+    pronote_url,
+    WebSpace::Students,
+    username,
+    password,
+    device_uuid.to_string()
+  ).await.unwrap();
+
+  println!("{}", serde_json::to_string_pretty(&pronote_session).unwrap());
+
+  // We do a lil' memory usage check at the end.
+  let peak_mem = PEAK_ALLOC.peak_usage_as_kb();
+  let current_mem = PEAK_ALLOC.current_usage_as_kb();
+	println!("This program currently uses {} KB of RAM, max was {} KB.", current_mem, peak_mem);
 }

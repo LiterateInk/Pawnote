0950b3c3b1d7c585b53a5de167600090d9b4dfd1
Mikkel ALMONTE--RINGAUD <mikkel@milescode.dev>
2024-07-19 22:52:57 +0200

    refactor(models): re-export everything using `*`

--- pawnote/src/models/mod.rs
+++ pawnote/src/models/mod.rs
@@ -1,5 +1,13 @@
+//! Models are where we're doing all the parsing.
+//! 
+//! This might be where we implement helper functions in the structs
+//! or we just rename the fields to have better and meaningful naming.
+
 mod session;
-pub use session::Session;
+pub use session::*;
 
 mod session_information;
-pub use session_information::SessionInformation;
+pub use session_information::*;
+
+mod instance_information;
+pub use instance_information::*;

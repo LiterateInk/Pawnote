2cdf14b125d8c6e6247d377d414582ea43f344e8
Mikkel ALMONTE--RINGAUD <mikkel@milescode.dev>
2024-07-19 23:42:05 +0200

    fix(instance_information): endpoint should start with `/`
    since pronote root url gives without trailing slash

--- pawnote/src/api/instance_information/request.rs
+++ pawnote/src/api/instance_information/request.rs
@@ -1,9 +1,9 @@
 use utilities::Request;
 
-const MOBILE_INFO_ENDPOINT: &str = "infoMobileApp.json?id=0D264427-EEFC-4810-A9E9-346942A862A4";
+const MOBILE_INFO_ENDPOINT: &str = "/infoMobileApp.json?id=0D264427-EEFC-4810-A9E9-346942A862A4";
 
 pub fn build_request (pronote_root_url: String) -> Request {
-  let pronote_url = pronote_root_url.clone();
+  let mut pronote_url = pronote_root_url.clone();
   pronote_url.push_str(MOBILE_INFO_ENDPOINT);
 
   Request {

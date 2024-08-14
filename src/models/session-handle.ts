843ea1345456a942b8a6421c24598e3cfc543421
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-14 04:43:33 +0200

    docs(models): add comments on session handle

--- src/models/session-handle.ts
+++ src/models/session-handle.ts
@@ -1,13 +1,32 @@
 import type { Fetcher } from "@literate.ink/utilities";
-import type { Queue } from "~/core/queue";
+import type { Queue } from "~/api/private/queue";
 import type { SessionInformation } from "./session-information";
 
 export interface SessionHandle {
+  /**
+   * Initialized after a login.
+   *
+   * Parsed response for a PRONOTE session.
+   * Contains metadata, AES keys, RSA modulus, and more.
+   */
   information: SessionInformation
-  instance: any // TODO
-  user: any // TODO
 
-  // Initialized using `createSessionHandle`.
+  /**
+   * Initialized after a login.
+   *
+   * Raw response for the instance parameters.
+   * Contains a lot of various data, very hard to predict.
+   */
+  instance: any
+
+  /**
+   * Initialized after a login.
+   *
+   * Raw response for the user parameters.
+   * Contains a lot of various data, very hard to predict.
+   */
+  user: any
+
   readonly queue: Queue
   readonly fetcher: Fetcher
 }

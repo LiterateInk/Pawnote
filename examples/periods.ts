18abfc895c6eb2a670e82a52440b3100737dacd0
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-18 21:11:17 +0200

    docs(examples): fix periods

--- examples/periods.ts
+++ examples/periods.ts
@@ -1,16 +1,17 @@
-import { authenticatePronoteCredentials, PronoteApiAccountId } from "../src";
+import * as pronote from "../src";
+import { credentials } from "./_credentials";
 
-(async () => {
-  const { periods } = await authenticatePronoteCredentials("https://demo.index-education.net/pronote", {
-    accountTypeID: PronoteApiAccountId.Student,
-    username: "demonstration",
-    password: "pronotevs",
-
-    // Because this is just an example, don't forget to change this.
-    deviceUUID: "my-device-uuid"
+void async function main () {
+  const handle = pronote.createSessionHandle();
+  await pronote.loginCredentials(handle, {
+    url: credentials.pronoteURL,
+    kind: pronote.AccountKind.STUDENT,
+    username: credentials.username,
+    password: credentials.password,
+    deviceUUID: credentials.deviceUUID
   });
 
-  periods.forEach((period) => {
-    console.info(period.name, "starts the", period.start.toLocaleString(), "ends the", period.end.toLocaleString());
-  });
-})();
+  for (const period of handle.instance.periods) {
+    console.info(period.name, "starts the", period.startDate.toLocaleString(), "ends the", period.endDate.toLocaleString());
+  }
+}();

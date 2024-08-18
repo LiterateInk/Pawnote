720d88d7e9af0e138a6a52c7d74e85b61ab4db46
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-18 21:07:42 +0200

    fix(api): add decode

--- src/api/private/user-parameters.ts
+++ src/api/private/user-parameters.ts
@@ -1,9 +1,10 @@
 import { RequestFN } from "~/core/request-function";
+import { decodeUserParameters } from "~/decoders/user-parameters";
 import type { SessionHandle } from "~/models";
 import { UserParameters } from "~/models/user-parameters";
 
 export const userParameters = async (session: SessionHandle): Promise<UserParameters> => {
   const request = new RequestFN(session, "ParametresUtilisateur", {});
   const response = await request.send();
-  return response.data.donnees;
+  return decodeUserParameters(response.data.donnees, session);
 };

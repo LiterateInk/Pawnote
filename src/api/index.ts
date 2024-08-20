73b7c37f0f442b7fd3cb2af66bfb0f723e34e424
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-20 10:38:00 +0200

    chore: exports

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,15 @@
 export * from "./helpers/clean-url";
 export * from "./helpers/login";
 export * from "./helpers/parse-timetable";
+export * from "./helpers/presence-interval";
 export * from "./helpers/session-handle";
 export * from "./helpers/week-number";
 
+export * from "./attendance";
 export * from "./geolocation";
 export * from "./homepage";
 export * from "./instance";
 export * from "./partner-url";
+export * from "./presence";
 export * from "./session-information";
 export * from "./timetable";

d552ed4b629c966f622cd9b26292591b60c99961
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-20 10:37:51 +0200

    docs(examples): explanation in `periods`

--- examples/periods.ts
+++ examples/periods.ts
@@ -2,8 +2,8 @@ import * as pronote from "../src";
 import { credentials } from "./_credentials";
 
 void async function main () {
-  const handle = pronote.createSessionHandle();
-  await pronote.loginCredentials(handle, {
+  const session = pronote.createSessionHandle();
+  await pronote.loginCredentials(session, {
     url: credentials.pronoteURL,
     kind: pronote.AccountKind.STUDENT,
     username: credentials.username,
@@ -11,7 +11,26 @@ void async function main () {
     deviceUUID: credentials.deviceUUID
   });
 
-  for (const period of handle.instance.periods) {
-    console.info(period.name, "starts the", period.startDate.toLocaleString(), "ends the", period.endDate.toLocaleString());
+  console.info("-- REGISTERED PERIODS");
+  console.info("Those are periods that are registered in the instance globally, they're not linked yet to tabs.");
+
+  for (const period of session.instance.periods) {
+    console.info("+", period.name, "starts the", period.startDate.toLocaleString(), "ends the", period.endDate.toLocaleString());
+  }
+
+  console.info("-- SCOPED PERIODS");
+  console.info("Those are periods defined for each tabs. That allows to know the available correct selections and also know which one is the current default one.");
+
+  for (const tabID of session.user.authorizations.tabs) {
+    // We're on a student account, there's only one resource.
+    const tab = session.user.resources[0].tabs.get(tabID);
+    if (!tab) {
+      console.warn(`Skipping tab ${tabID} since no period is required there.`);
+      continue;
+    }
+
+    // Note that `tab.location` is the same as `tabID`.
+    console.info("+", tab.location, "=>", tab.defaultPeriod?.name ?? "(no default period)");
+    console.info("(also available:", tab.periods.map((period) => period.name).join(", "), "for this tab.)");
   }
 }();

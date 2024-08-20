21e0bea55bfff2b792685cf2be434828379ce174
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-20 10:31:52 +0200

    feat: add TabLocation to know the ID of tabs

---
+++ src/models/tab-location.ts
@@ -0,0 +1,15 @@
+export const TabLocation = {
+  Grades: 198,
+  Resources: 89,
+  Homework: 88,
+  Timetable: 16,
+  Evaluations: 201,
+  Account: 49,
+  Presence: 7,
+  News: 8,
+  Attendance: 19,
+  Discussions: 131,
+  GradesReport: 13
+} as const;
+
+export type TabLocation = typeof TabLocation[keyof typeof TabLocation];

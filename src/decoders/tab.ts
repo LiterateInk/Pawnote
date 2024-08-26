80841d11b68f864f865af8020439df9c9649ba1c
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-27 00:07:23 +0200

    fix(decoders): periods not read correctly

--- src/decoders/tab.ts
+++ src/decoders/tab.ts
@@ -4,7 +4,7 @@ export const decodeTab = (tab: any, instancePeriods: Period[]): Tab => {
   const find = (id: string) => instancePeriods.find((p2) => p2.id === id);
 
   const defaultPeriod = tab.periodeParDefaut && find(tab.periodeParDefaut.V.N);
-  const periods = tab.listePeriodes.V.map((p1: any) => find(p1)).filter(Boolean);
+  const periods = tab.listePeriodes.V.map((p1: any) => find(p1.N)).filter(Boolean);
 
   return {
     defaultPeriod,

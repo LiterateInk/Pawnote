7dc1c2c990347cb9e2724ca08378d31447b8b7bb
Maël GUERIN <mael.guerin@murena.io>
2024-09-02 12:10:27 +0200

    feat(api): add `session-information` request

--- library/src/commonMain/kotlin/models/AccountKind.kt
+++ library/src/commonMain/kotlin/models/AccountKind.kt
@@ -3,5 +3,9 @@ package models
 enum class AccountKind (val code: Int) {
     STUDENT(6),
     PARENT(7),
-    TEACHER(8)
+    TEACHER(8);
+
+    companion object {
+        fun fromInt(code: Int) = entries.first {it.code == code}
+    }
 }
--- library/src/commonMain/kotlin/models/SessionAccessKind.kt
+++ library/src/commonMain/kotlin/models/SessionAccessKind.kt
@@ -6,5 +6,9 @@ enum class SessionAccessKind {
     DIRECT_CONNECTION,
     TOKEN_ACCOUNT_CONNECTION,
     TOKEN_DIRECT_CONNECTION,
-    COOKIE_CONNECTION
+    COOKIE_CONNECTION;
+
+    companion object {
+        fun fromInt(ordinal: Int) = entries.first {it.ordinal == ordinal}
+    }
 }

c7bec3f03bee8e6334da4387fa5ae373041b8433
Maël GUERIN <mael.guerin@murena.io>
2024-09-02 14:18:50 +0200

    feat(decoders): add domain decoder

---
+++ library/src/commonMain/kotlin/decoders/domain.kt
@@ -0,0 +1,20 @@
+package decoders
+
+fun decodeDomain (api: String): List<Int> {
+    if (api[0] != '[' || api[api.length - 1] != ']') return listOf()
+
+    val cleanedApi = api.slice(1 downTo 0)
+    val output: MutableList<Int> = mutableListOf()
+
+    for (part in cleanedApi.split(',')) {
+        if (part.contains("..")) {
+            val indices = part.split("..").map { n -> n.toInt() }
+            val start = indices.first()
+            val end = indices.last()
+
+            for (i in start..end) output.add(i)
+        }
+    }
+
+    return output.toList()
+}

00aa07ac0670bfe109b0ed16307be0cda1fc2e1d
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-05 00:15:46 +0200

    docs(examples/auth): rewrite the whole student example to work with 2FAs

--- examples/authentication/student.ts
+++ examples/authentication/student.ts
@@ -1,5 +1,86 @@
 import * as pronote from "../../src";
 import { credentials } from "../_credentials";
+import { select } from "@inquirer/prompts";
+
+// Defaults for this example !
+const _customPassword = "1234567890";
+const _source = "TEST_PAWNOTE";
+const _pin = "0000";
+
+const selectMode = async (): Promise<pronote.DoubleAuthMode> => {
+  const mode = await select({
+    message: "What double-authentication mode do you want to use?",
+    choices: [
+      {
+        name: "PIN",
+        value: pronote.DoubleAuthMode.MGDA_SaisieCodePIN
+      },
+      {
+        name: "Notification",
+        value: pronote.DoubleAuthMode.MGDA_NotificationSeulement
+      },
+      {
+        name: "Nothing",
+        value: pronote.DoubleAuthMode.MGDA_Inactive
+      }
+    ]
+  });
+
+  return mode;
+};
+
+const handleSecurity = async (session: pronote.SessionHandle, error: pronote.SecurityError): Promise<pronote.RefreshInformation> => {
+  if (error instanceof pronote.SecurityError) {
+    let mode: pronote.DoubleAuthMode | undefined;
+    let password: string | undefined;
+    let pin: string | undefined;
+
+    if (error.handle.shouldCustomPassword) {
+      console.warn(`=> Set password to '${_customPassword}'`);
+      password = _customPassword;
+
+      const isPasswordCorrect = await pronote.securityCheckCustomPassword(session, password);
+      if (!isPasswordCorrect) throw new Error(`Password '${password}' cannot be set on your instance, it should match the 'rules' policy.`);
+    }
+
+    if (error.handle.shouldCustomDoubleAuth) {
+      mode = await selectMode();
+
+      if (mode === pronote.DoubleAuthMode.MGDA_SaisieCodePIN) {
+        console.warn(`=> Set security strategy to PIN with '${_pin}' as code`);
+        pin = _pin;
+      }
+    }
+
+    if (error.handle.shouldEnterPIN) {
+      console.warn(`=> PIN required, will use '${_pin}'`);
+      pin = _pin;
+
+      const isPinCorrect = await pronote.securityCheckPIN(session, pin);
+      if (!isPinCorrect) throw new Error("The PIN is incorrect");
+    }
+
+    const deviceName = _source;
+    console.warn(`=> '${deviceName}' was registered as trusted source to authenticate`);
+
+    const isAlreadyKnown = await pronote.securitySource(session, deviceName);
+    if (isAlreadyKnown) console.warn("The source is already known, registration will be ignored.");
+
+    // This is the function that will save/confirm the security settings.
+    await pronote.securitySave(session, error.handle, {
+      mode,
+      pin,
+      password,
+      deviceName
+    });
+
+    // We finish the login manually by calling the last authentication method.
+    const context = error.handle.context;
+    return pronote.finishLoginManually(session, context.authentication, context.identity, context.initialUsername);
+  }
+
+  throw error;
+};
 
 void async function main () {
   console.log("------ CREDENTIALS:");
@@ -12,49 +93,7 @@ void async function main () {
     username: credentials.username,
     password: credentials.password,
     deviceUUID: credentials.deviceUUID
-  }).catch(async (error) => {
-    if (error instanceof pronote.SecurityError) {
-      console.warn("Double authentication is required, we're going to handle it automatically for you.");
-      console.warn("Please note that it only works if the password rules are all disabled and length is set on minimum 10 !");
-
-      // Load defaults.
-      const customPassword = "1234567890";
-      const source = "test_" + Date.now();
-      const pin = "1234";
-
-      if (error.handle.shouldCustomPassword) {
-        // TODO: add a prompt that follows the password rules
-        const correct = await pronote.securityCheckCustomPassword(session, customPassword);
-        console.log({ correct});
-
-        if (!correct) throw new Error("Your instance need a password more secure.");
-      }
-
-      if (error.handle.shouldCustomDoubleAuth) {
-        if (error.handle.availableSecurityModes.includes(pronote.DoubleAuthMode.MGDA_SaisieCodePIN)) {
-          const alreadyKnown = await pronote.securitySource(session, source);
-          console.log({ alreadyKnown });
-          // TODO: add a prompt for PIN
-        }
-        else if (error.handle.availableSecurityModes.includes(pronote.DoubleAuthMode.MGDA_NotificationSeulement)) {
-          // not reversed yet
-          throw error;
-        }
-      }
-
-      await pronote.securitySave(session, {
-        password: customPassword,
-        deviceName: source,
-        pin,
-        mode: pronote.DoubleAuthMode.MGDA_SaisieCodePIN
-      });
-
-      const context = error.handle.context;
-      return pronote.finishLoginManually(session, context.authentication, context.identity, context.initialUsername);
-    }
-
-    throw error;
-  });
+  }).catch((error) => handleSecurity(session, error));
 
   console.info("Username:", refresh.username);
   console.info("Token:", refresh.token);
@@ -64,18 +103,19 @@ void async function main () {
     // We login now using the token to prove the point.
     console.log("\n------ TOKEN:");
 
-    const next_handle = pronote.createSessionHandle();
+    const nextSession = pronote.createSessionHandle();
 
-    const next_refresh = await pronote.loginToken(next_handle, {
+    const nextRefresh = await pronote.loginToken(nextSession, {
       url: refresh.url,
       kind: refresh.kind,
       username: refresh.username,
       token: refresh.token,
-      deviceUUID: credentials.deviceUUID
-    });
+      deviceUUID: credentials.deviceUUID,
+      navigatorIdentifier: refresh.navigatorIdentifier
+    }).catch((error) => handleSecurity(nextSession, error));
 
-    console.info("Username:", next_refresh.username);
-    console.info("Next-Time Token:", next_refresh.token);
+    console.info("Username:", nextRefresh.username);
+    console.info("Next-Time Token:", nextRefresh.token);
   }
   else console.info("\nYou're connected to a demonstration instance, thus you're not able to use the next-time token.");
 }();

c2b3c856d68f0d4a82f4d27bdda06e18b38985d6
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-04 08:31:18 +0200

    fix(decoders): move constant so it doesn't throw depending on import order

--- src/decoders/security-modal.ts
+++ src/decoders/security-modal.ts
@@ -2,21 +2,21 @@ import { decodeDomain } from "./domain";
 import { decodePasswordRules } from "./password-rules";
 import { DoubleAuthClientAction, type DoubleAuthMode, type SecurityModal } from "~/models";
 
-const DOUBLE_AUTH_POSSIBILITIES = [
-  DoubleAuthClientAction.AIHMSC_ChoixStrategie,
-  DoubleAuthClientAction.AIHMSC_SaisieCodePINetSource,
-  DoubleAuthClientAction.AIHMSC_SaisieSourcePourNotifSeulement
-] as DoubleAuthClientAction[];
-
 export const decodeSecurityModal = (authentication: any, identity: any, initialUsername?: string): SecurityModal => {
   const actions = decodeDomain(authentication.actionsDoubleAuth.V) as DoubleAuthClientAction[];
+
+  const doubleAuthPossibilities = [
+    DoubleAuthClientAction.AIHMSC_ChoixStrategie,
+    DoubleAuthClientAction.AIHMSC_SaisieCodePINetSource,
+    DoubleAuthClientAction.AIHMSC_SaisieSourcePourNotifSeulement
+  ] as DoubleAuthClientAction[];
 
   return {
     availableSecurityModes: decodeDomain(authentication.modesPossibles.V) as DoubleAuthMode[],
     defaultSecurityMode: authentication.modeSecurisationParDefaut,
     passwordRules: decodePasswordRules(authentication.reglesSaisieMDP),
     shouldCustomPassword: actions.includes(DoubleAuthClientAction.AIHMSC_PersonnalisationMotDePasse),
-    shouldCustomDoubleAuth: actions.some((action) => DOUBLE_AUTH_POSSIBILITIES.includes(action)),
+    shouldCustomDoubleAuth: actions.some((action) => doubleAuthPossibilities.includes(action)),
 
     context: {
       authentication,

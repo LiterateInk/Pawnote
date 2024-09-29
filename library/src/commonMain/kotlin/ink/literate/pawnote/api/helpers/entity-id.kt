c55115d5d1c8a5791f6e9bf4382aa6cb90e6b74b
Maël GUERIN <mael.guerin@murena.io>
2024-09-29 17:26:55 +0200

    feat(api): add assignment file upload

---
+++ library/src/commonMain/kotlin/ink/literate/pawnote/api/helpers/entity-id.kt
@@ -0,0 +1,12 @@
+package ink.literate.pawnote.api.helpers
+
+/**
+ * Defaults to `-1000`, used to generate IDs for newly created objects.
+ * Whenever an object is created using this ID, it should be decremented by `1`.
+ */
+var id = -1000
+
+fun createEntityID(): Int {
+    id -= 1
+    return id
+}
---
+++ library/src/commonMain/kotlin/ink/literate/pawnote/models/errors/upload.kt
@@ -0,0 +1,5 @@
+package ink.literate.pawnote.models.errors
+
+class UploadSizeError (maxSizeInBytes: Double) : Error("The file you are trying to upload is too large, maximum allowed is $maxSizeInBytes bytes")
+
+class UploadFailedError : Error("The file upload failed")

b8ed9ad52b0713a32d85e8c861e98f383fbbad71
Maël GUERIN <mael.guerin@murena.io>
2024-10-06 10:30:25 +0200

    feat(api): implement `discussion-remote-mutate-draft`

---
+++ library/src/commonMain/kotlin/ink/literate/pawnote/api/discussion-remote-mutate-draft.kt
@@ -0,0 +1,24 @@
+package ink.literate.pawnote.api
+
+import ink.literate.pawnote.api.private.discussionPostCommand
+import ink.literate.pawnote.models.Discussion
+import ink.literate.pawnote.models.DiscussionCommand
+import ink.literate.pawnote.models.DiscussionDraftMessage
+import ink.literate.pawnote.models.SessionHandle
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+
+/**
+ * Send local changes to the object
+ * to the server.
+ */
+suspend fun discussionRemoteMutateDraft (session: SessionHandle, discussion: Discussion, draft: DiscussionDraftMessage) {
+    discussionPostCommand(session, DiscussionCommand.brouillon, buildJsonObject {
+        put("id", draft.possessionID)
+        put("content", draft.content)
+        put("replyMessageID", draft.replyMessageID)
+    })
+
+    discussions(session, discussion.cache)
+    discussionMessages(session, discussion)
+}

c22a892bc4c6ce95f2f50fc5d69059041399627b
Maël GUERIN <mael.guerin@murena.io>
2024-10-06 10:37:48 +0200

    feat(api): implement `discussion-restore-trash`

---
+++ library/src/commonMain/kotlin/ink/literate/pawnote/api/discussion-restore-trash.kt
@@ -0,0 +1,15 @@
+package ink.literate.pawnote.api
+
+import ink.literate.pawnote.api.private.discussionPostCommand
+import ink.literate.pawnote.models.Discussion
+import ink.literate.pawnote.models.DiscussionCommand
+import ink.literate.pawnote.models.SessionHandle
+import kotlinx.serialization.json.buildJsonObject
+
+suspend fun discussionRestoreTrash (session: SessionHandle, discussion: Discussion) {
+    discussionPostCommand(session, DiscussionCommand.Restore, buildJsonObject {
+        put("possessions", discussion.possessions)
+    })
+
+    discussions(session, discussion.cache)
+}
